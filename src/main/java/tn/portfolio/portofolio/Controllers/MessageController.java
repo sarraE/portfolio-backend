a05f6a62b21a0d22a9320654474adb3af5fccdd6
sarraE <160863647+sarraE@users.noreply.github.com>
2026-04-29 14:05:18 +0100

    Update MessageController.java

--- src/main/java/tn/portfolio/portofolio/Controllers/MessageController.java
+++ src/main/java/tn/portfolio/portofolio/Controllers/MessageController.java
@@ -13,7 +13,7 @@ import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @RestController
 @AllArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-frontend-pznn.vercel.app")
 public class MessageController {
     private IMessageService iMessageService;
 
