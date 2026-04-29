52ab605f738389b2fc0a13c1102e2c4c07f3d9aa
sarraE <160863647+sarraE@users.noreply.github.com>
2026-04-29 19:54:55 +0100

    Update EmailService.java

--- src/main/java/tn/portfolio/portofolio/Services/EmailService.java
+++ src/main/java/tn/portfolio/portofolio/Services/EmailService.java
@@ -13,16 +13,22 @@ public class EmailService {
     private JavaMailSender mailer;
 
     @Async
-    public void sendEmail(Message message) {
+   public void sendEmail(Message message) {
+    try {
         SimpleMailMessage email = new SimpleMailMessage();
         email.setTo("sarraelkamel99@gmail.com");
         email.setReplyTo(message.getEmail());
         email.setSubject("Nouveau message : " + message.getSubject());
         email.setText(
-                "Nom : " + message.getName() + "\n" +
-                        "Email : " + message.getEmail() + "\n\n" +
-                        message.getContent()
+            "Nom : " + message.getName() + "\n" +
+            "Email : " + message.getEmail() + "\n\n" +
+            message.getContent()
         );
         mailer.send(email);
+        System.out.println("✅ Email envoyé avec succès !");
+    } catch (Exception e) {
+        System.err.println("❌ Erreur envoi email: " + e.getMessage());
+        e.printStackTrace();
     }
+}
 }
