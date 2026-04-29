daafeef42ee5d8dd366c18c6094106898c8e5c35
sarraE <160863647+sarraE@users.noreply.github.com>
2026-04-29 19:21:07 +0100

    Update Message.java

--- src/main/java/tn/portfolio/portofolio/Entities/Message.java
+++ src/main/java/tn/portfolio/portofolio/Entities/Message.java
@@ -24,6 +24,7 @@ public class Message implements Serializable {
     LocalDateTime dispatchDate;
     boolean read;
 
-    @ManyToOne
+    @ManyToOne(optional = true)
+    @JoinColumn(name = "project_id_project", nullable = true)
     Project project;
 }
