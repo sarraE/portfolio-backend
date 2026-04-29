e5cc44e3c45f6f85a5678585a645cd8d2c08dfbc
sarraE <160863647+sarraE@users.noreply.github.com>
2026-04-29 18:27:28 +0100

    Update PortofolioApplication.java

--- src/main/java/tn/portfolio/portofolio/PortofolioApplication.java
+++ src/main/java/tn/portfolio/portofolio/PortofolioApplication.java
@@ -2,8 +2,10 @@ package tn.portfolio.portofolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PortofolioApplication {
 
     public static void main(String[] args) {
