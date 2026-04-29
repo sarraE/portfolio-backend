8a09a2b84edf81253286b7d4032572a45f034929
sarraE <160863647+sarraE@users.noreply.github.com>
2026-04-29 17:26:25 +0100

    Update ProjectController.java

--- src/main/java/tn/portfolio/portofolio/Controllers/ProjectController.java
+++ src/main/java/tn/portfolio/portofolio/Controllers/ProjectController.java
@@ -15,7 +15,7 @@ import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @RestController
 @AllArgsConstructor
-@CrossOrigin(origins = "https://portfolio-frontend-pznn.vercel.app")
+
 public class ProjectController {
     public IProjectService iProjectService;
 
